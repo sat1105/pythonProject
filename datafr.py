b84c3d72915d36a54896a02d0ab4c8d6f97d5212
sat1105 <gssatishgowda@gmail.com>
2024-03-06 14:39:35 +0530

    imported when and col

--- datafr.py
+++ datafr.py
@@ -1,9 +1,12 @@
 import pyspark
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col,aggregate,when
 
 sc = SparkContext()
 spark = SparkSession.builder.appName("parallelizedf").master("local").getOrCreate()
+
+
 
 
 
